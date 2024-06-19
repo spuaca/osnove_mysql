5f4245d9bc98ba3fbc17493c756d5664da223e8c
Stjepan Puača <stipe.puaca@gmail.com>
2024-06-19 14:00:40 +0200

    renamed zadaca2.sql to zadaca_library & modified videoteka_select.sql

--- videoteka_select.sql
+++ videoteka_select.sql
@@ -55,11 +55,69 @@ SELECT f.naslov, f.godina, z.ime
     FROM filmovi f
     JOIN zanrovi z ON f.zanr_id = z.id;
 
+-- Spoji tablice filmovi i zanrovi kako bi u rezultatu dobio skupljene podatke iz obje tablice
 SELECT f.naslov, f.godina, z.ime AS 'Zanr'
     FROM filmovi f
     JOIN zanrovi z ON f.zanr_id = z.id;
 
+-- Spoji tablice filmovi, zanrovi i cjenik kako bi u rezultatu dobio skupljene podatke iz sve tri tablice
 SELECT f.naslov, f.godina, z.ime, c.tip_filma AS 'Tip', c.cijena, c.zakasnina_po_danu AS 'Zakasnina'
     FROM filmovi f
     JOIN zanrovi z ON f.zanr_id = z.id
     JOIN cjenik c ON f.cjenik_id = c.id;
+
+-- spoji tablice posudba sa filmovi, clanovi i mediji preko vezne tablice zaliha
+SELECT p.datum_posudbe, p.datum_povrata, c.ime, f.naslov, m.tip
+    FROM posudba p
+    JOIN clanovi c ON p.clan_id = c.id
+    JOIN zaliha z ON p.zaliha_id = z.id
+    JOIN filmovi f ON z.film_id = f.id
+    JOIN mediji m ON z.medij_id = m.id;
+
+-- izlistaj i film naziv i medij za filmove koji nisu vraceni
+SELECT p.datum_posudbe, p.datum_povrata, c.ime, f.naslov, m.tip
+    FROM posudba p
+    JOIN clanovi c ON p.clan_id = c.id
+    JOIN zaliha z ON p.zaliha_id = z.id
+    JOIN filmovi f ON z.film_id = f.id
+    JOIN mediji m ON z.medij_id = m.id
+    WHERE p.datum_povrata IS NULL;
+
+-- povezi sve tablice i izlistaj podatke
+SELECT p.datum_posudbe,
+       p.datum_povrata,
+       c.ime AS "Ime Clana",
+       f.naslov,
+       m.tip AS Medij,
+       zanrovi.ime AS Zanr,
+       cj.tip_filma,
+       cj.cijena,
+       cj.zakasnina_po_danu
+FROM posudba p
+    JOIN clanovi c ON p.clan_id = c.id
+    JOIN zaliha z ON p.zaliha_id = z.id
+    JOIN filmovi f ON z.film_id = f.id
+    JOIN mediji m ON z.medij_id = m.id
+    JOIN zanrovi ON zanrovi.id = f.zanr_id
+    JOIN cjenik cj ON cj.id = f.cjenik_id
+    WHERE p.datum_povrata IS NOT NULL;
+
+-- ispisi clanove koji su posudili vise od jednog filma
+SELECT c.ime
+FROM clanovi c
+JOIN posudba p ON p.clan_id = c.id
+GROUP BY c.ime
+HAVING COUNT(c.id) > 1;
+
+-- GROUP BY ispisite totalnu kolicinu kopija dostupnih po filmu
+SELECT f.*, SUM(z.kolicina) AS "Ukupna kolicina"
+FROM filmovi f
+JOIN zaliha z ON z.film_id = f.id
+GROUP BY f.id;
+
+SELECT COUNT(DISTINCT clan_id) FROM posudba;
+
+SELECT * FROM posudba p
+JOIN clanovi c
+WHERE c.id = p.clan_id
+WHERE COUNT(c.id) BETWEEN 1 AND 60;
